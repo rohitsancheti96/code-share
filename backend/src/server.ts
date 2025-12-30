ed89453cd54fba388ec2d70142475215b5b524da
Rohit Sancheti <rohitsancheti96@gmail.com>
2025-12-30 09:55:57 +0530

    vercel specific changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { ENV } from './lib/env';
+import { ENV } from './lib/env.js';
  
 const app = express();
 
@@ -11,6 +11,12 @@ app.get("/health", (req, res) => {
     res.status(200).json({ message: "OK" });
 })
 
-app.listen(ENV.PORT || 3000, () => {
-    console.log('Server is running on port ' + ENV.PORT || 3000);
-});
+
+if (!process.env.VERCEL) {
+    const port = ENV.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
+}
+
+export default app;
